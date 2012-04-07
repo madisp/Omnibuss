35c928f88c66fa50b6ba6390d3193b2f44c53498
Tauno Talimaa <tauntz@gmail.com>
2012-04-08 01:13:16 +0300

    Map clustering changes

--- Omnibuss/PushpinContainer.cs
+++ Omnibuss/PushpinContainer.cs
@@ -14,60 +14,71 @@ using System.Collections.Generic;
 
 namespace MarkerClustering
 {
-  /// <summary>
-  /// A container for one or more pushpins at a given screen coordinate.
-  /// </summary>
-  public class PushpinContainer
-  {
-    private List<Pushpin> _pushpins = new List<Pushpin>();
-
     /// <summary>
-    /// Creates a container for the given pushpin
+    /// A container for one or more pushpins at a given screen coordinate.
     /// </summary>
-    public PushpinContainer(Pushpin pushpin, Point location)
+    public class PushpinContainer
     {
-      _pushpins.Add(pushpin);
-      ScreenLocation = location;
-    }
+        private List<Pushpin> _pushpins = new List<Pushpin>();
+        private Map _map;
 
-    /// <summary>
-    /// Adds the pins from the given container
-    /// </summary>
-    public void Merge(PushpinContainer pinContainer)
-    {
-      foreach (var pin in pinContainer._pushpins)
-      {
-        _pushpins.Add(pin);
-      }
-    }
-
-    /// <summary>
-    /// Gets or sets the current screen location of this container
-    /// </summary>
-    public Point ScreenLocation { get; private set; }
-
-    /// <summary>
-    /// Gets the visual representation of the contents of this container. If it is 
-    /// a single pushpin, the pushpin itself is returned. If multiple pushpins are present
-    /// a pushpin with the given clusterTemplate is returned.
-    /// </summary>
-    public FrameworkElement GetElement(DataTemplate clusterTemplate)
-    {
-      if (_pushpins.Count == 1)
-      {
-        return _pushpins[0];
-      }
-      else
-      {
-        return new Pushpin()
+        /// <summary>
+        /// Creates a container for the given pushpin
+        /// </summary>
+        public PushpinContainer(Pushpin pushpin, Point location, Map map)
         {
-          Location = _pushpins.First().Location,
-          Content = _pushpins.Select(pin => pin.DataContext).ToList(),
-          ContentTemplate = clusterTemplate,
-          Background = new SolidColorBrush(Colors.Red)
-        };
-      }
+            _map = map;
+            _pushpins.Add(pushpin);
+            ScreenLocation = location;
+        }
+
+        /// <summary>
+        /// Adds the pins from the given container
+        /// </summary>
+        public void Merge(PushpinContainer pinContainer)
+        {
+            foreach (var pin in pinContainer._pushpins)
+            {
+                _pushpins.Add(pin);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current screen location of this container
+        /// </summary>
+        public Point ScreenLocation { get; private set; }
+
+        /// <summary>
+        /// Gets the visual representation of the contents of this container. If it is 
+        /// a single pushpin, the pushpin itself is returned. If multiple pushpins are present
+        /// a pushpin with the given clusterTemplate is returned.
+        /// </summary>
+        public FrameworkElement GetElement(DataTemplate clusterTemplate)
+        {
+            if (_pushpins.Count == 1)
+            {
+                return _pushpins[0];
+            }
+            else
+            {
+                Pushpin retPin = new Pushpin()
+                {
+                    Location = _pushpins.First().Location,
+                    Content = _pushpins.Select(pin => pin.DataContext).ToList(),
+                    ContentTemplate = clusterTemplate,
+                    Background = new SolidColorBrush((Color)Application.Current.Resources["PhoneAccentColor"])
+
+                };
+                
+                retPin.MouseLeftButtonUp += new MouseButtonEventHandler(
+                        (object sender, MouseButtonEventArgs e) =>
+                        {
+                            _map.Center = retPin.Location;
+                            _map.ZoomLevel = _map.ZoomLevel + 3;
+                        });
+                return retPin;
+            }
+        }
+
     }
-    
-  }
 }
