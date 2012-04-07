320475c43d34b382ccbba48675000d8dcfae7b91
Madis Pink <madis.pink@gmail.com>
2012-04-07 03:46:16 +0300

    Get trips, get stops

--- Omnibuss/OmnibussModel.cs
+++ Omnibuss/OmnibussModel.cs
@@ -11,6 +11,7 @@ using System.Windows.Shapes;
 using System.Collections.Generic;
 using System.Data.Linq;
 using System.Linq;
+using System.Collections;
 
 namespace Omnibuss
 {
@@ -47,6 +48,71 @@ namespace Omnibuss
         public Route GetRoute(UInt32 id)
         {
             return (from Route route in db.Routes where route.Route_id.Equals(id) select route).Single();
+        }
+
+        public List<Trip> GetTripsByRoute(Route route)
+        {
+            return (from trip in db.Trips where trip.Route_id.Equals(route.Route_id) select trip).ToList();
+        }
+
+        public List<Stop> GetStopsByTrip(Trip trip)
+        {
+            List<Point> stopTimes = (
+                from stop_time in db.Stop_times
+                join stop in db.Stops on stop_time.Stop_id equals stop.Id
+                where stop_time.Trip_id.Equals(trip.Trip_id)
+                select new Point { StopId = stop.Id, Sequence = stop_time.Stop_sequence, Latitude = stop.Latitude, Longitude = stop.Longitude }
+            ).Distinct().OrderBy(pt => pt.Sequence).ToList();
+
+            List<Stop> ret = new List<Stop>();
+
+            foreach (Point time in stopTimes)
+            {
+                ret.Add(GetStop((uint)time.StopId));
+            }
+
+            return ret;
+        }
+    }
+
+    internal class Comparer : IComparer<Point>
+    {
+        int System.Collections.Generic.IComparer<Point>.Compare(Point x, Point y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class ProjectionComparer<TElement, TKey> : IComparer<TElement>
+    {
+        private readonly Func<TElement, TKey> keySelector;
+        private readonly IComparer<TKey> comparer;
+
+        internal ProjectionComparer(Func<TElement, TKey> keySelector,
+            IComparer<TKey> comparer)
+        {
+            this.keySelector = keySelector;
+            this.comparer = comparer ?? Comparer<TKey>.Default;
+        }
+
+        public int Compare(TElement x, TElement y)
+        {
+            TKey keyX = keySelector(x);
+            TKey keyY = keySelector(y);
+            return comparer.Compare(keyX, keyY);
+        }
+    }
+
+    public class Point : IEquatable<Point>
+    {
+        public int? StopId;
+        public int? Sequence;
+        public double? Latitude;
+        public double? Longitude;
+
+        public bool Equals(Point other)
+        {
+            return StopId == other.StopId;
         }
     }
 }
