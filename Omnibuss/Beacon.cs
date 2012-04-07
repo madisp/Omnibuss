08496911f13d43bccc6414badaf486eaf36909f8
Tauno Talimaa <tauntz@gmail.com>
2012-04-07 19:31:52 +0300

    Added route drawing

---
+++ Omnibuss/Beacon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Omnibuss
+{
+    public class Beacon
+    {
+
+        public virtual int Id { get; set; }
+        public virtual DateTime Timestamp { get; set; }
+        public virtual string Description { get; set; }
+        public virtual string Type { get; set; }
+        public virtual double DistanceFromCustomer { get; set; }
+
+        public override string ToString()
+        {
+            return Timestamp + " " + Description;
+        }
+    }
+}
