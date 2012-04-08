f0e6ded34bdfea11fc7a35d50fa96d408b2e4141
Tauno Talimaa <tauntz@gmail.com>
2012-04-08 09:15:34 +0300

    Added id/doc nr inputs

--- Omnibuss/TicketPage.xaml.cs
+++ Omnibuss/TicketPage.xaml.cs
@@ -10,14 +10,37 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using System.IO.IsolatedStorage;
+using System.Diagnostics;
+
 
 namespace Omnibuss
 {
     public partial class TicketPage : PhoneApplicationPage
     {
+
+        string isikukood;
+        string dokNumber;
+
         public TicketPage()
         {
             InitializeComponent();
+            var x = IsolatedStorageSettings.ApplicationSettings.TryGetValue("isikukood", out isikukood);
+            var y = IsolatedStorageSettings.ApplicationSettings.TryGetValue("dokNr", out dokNumber);
+            if (isikukood == null || dokNumber == null || isikukood.Length == 0 || dokNumber.Length == 0) {
+                Debug.WriteLine("Show inputs");
+                DataPanel.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            IsolatedStorageSettings.ApplicationSettings.Add("isikukood", idInput.Text);
+            IsolatedStorageSettings.ApplicationSettings.Add("dokNr", docNrInput.Text);
+            var x = IsolatedStorageSettings.ApplicationSettings.TryGetValue("isikukood", out isikukood);
+            var y = IsolatedStorageSettings.ApplicationSettings.TryGetValue("dokNr", out dokNumber);
+            Debug.WriteLine("Hei:" + isikukood + " / " + dokNumber + "!");
+            DataPanel.Visibility = System.Windows.Visibility.Collapsed;
         }
     }
 }
